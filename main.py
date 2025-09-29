7924d450a34d97e4795391f08eebd4dcfb02e7a0
qxz2pp2 <reka.kelemen@partner.bmw.de>
2025-09-29 12:34:38 +0300

    section 3

--- main.py
+++ main.py
@@ -3,17 +3,16 @@ from langchain_core.prompts import PromptTemplate
 from langchain_openai import ChatOpenAI
 from langchain_ollama import ChatOllama
 
-import requests
-from langsmith.client import Client
-
-# requests.packages.urllib3.disable_warnings()
-# client = Client(verify_ssl=False)
-
+# section 3
+from langchain import hub
+from langchain.agents import AgentExecutor
+# runtime of agent, the executor makes the calls
+from langchain.agents.react.agent import create_react_agent
+from langchain_tavily import TavilySearch
 
 load_dotenv()
 
-
-def main():
+def section2():
     print("Hello from langchain-course!")
     information = """Elon Reeve Musk (/ˈiːlɒn/ EE-lon; born June 28, 1971) is an international businessman and entrepreneur known for his leadership of Tesla, SpaceX, X (formerly Twitter), and the Department of Government Efficiency (DOGE). Musk has been the wealthiest person in the world since 2021; as of May 2025, Forbes estimates his net worth to be US$424.7 billion.
 
@@ -40,6 +39,29 @@ Musk's political activities, views, and statements have made him a polarizing fi
     chain = summary_prompt_template | llm
     response = chain.invoke(input={"information": information})
     print(response.content)
+
+def section3():
+    print("Section 3 - Search Agents")
+    tools = [TavilySearch()]
+    llm = ChatOpenAI(model="gpt-4")
+    react_prompt = hub.pull("hwchase17/react")
+    agent = create_react_agent(
+        llm,
+        tools=tools,
+        prompt=react_prompt,
+    )
+    agent_executer = AgentExecutor(agent=agent, tools=tools, verbose=True)
+    chain = agent_executer
+    result = chain.invoke(
+        input={
+            "input": "search for 3 job postings for an ai engineer using langchain in the bay area on linkedin and list their details"
+        }
+    )
+    print(result)
+
+
+def main():
+    section3()
 
 if __name__ == "__main__":
     main()
