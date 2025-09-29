86b2862a999cde3357fdf15582ec2cd486b695a4
qxz2pp2 <reka.kelemen@partner.bmw.de>
2025-09-29 13:29:35 +0300

    use with_structured_output instead of output parsing

--- main.py
+++ main.py
@@ -51,24 +51,32 @@ def section3():
     print("Section 3 - Search Agents")
     tools = [TavilySearch()]
     llm = ChatOpenAI(model="gpt-4")
+    structured_llm = llm.with_structured_output(AgentResponse)
     react_prompt = hub.pull("hwchase17/react")
-    output_parser = PydanticOutputParser(pydantic_object=AgentResponse)
+    # output_parser = PydanticOutputParser(pydantic_object=AgentResponse)
+
+    # react_prompt_with_format_instructions = PromptTemplate(
+    #     template=REACT_PROMPT_WITH_FORMAT_INSTRUCTIONS,
+    #     input_variables=["input", "agent_scratchpad", "tool_names"]
+    # ).partial(format_instructions=output_parser.get_format_instructions())
     react_prompt_with_format_instructions = PromptTemplate(
         template=REACT_PROMPT_WITH_FORMAT_INSTRUCTIONS,
         input_variables=["input", "agent_scratchpad", "tool_names"]
-    ).partial(format_instructions=output_parser.get_format_instructions())
+    ).partial(format_instructions="")
 
 
     agent = create_react_agent(
-        llm,
+        llm=llm,
         tools=tools,
         prompt=react_prompt_with_format_instructions,
     )
     agent_executer = AgentExecutor(agent=agent, tools=tools, verbose=True)
     extract_output = RunnableLambda(lambda x: x["output"])
-    parse_output = RunnableLambda(lambda x: output_parser.parse(x))
-    chain = agent_executer | extract_output | parse_output
-    
+    # parse_output = RunnableLambda(lambda x: output_parser.parse(x))
+    # chain = agent_executer | extract_output | parse_output
+    # instead of output parser use the structured llm   
+    chain = agent_executer | extract_output | structured_llm
+
     result = chain.invoke(
         input={
             "input": "search for 3 job postings for an ai engineer using langchain in the bay area on linkedin and list their details"
